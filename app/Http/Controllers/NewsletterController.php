18b99365ce0a1f99c115c34afc75bcf5545af739
Fray Dalton <fray117@exploiter.id>
2023-06-28 00:39:08 +0700

    update newsletter

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -8,24 +8,42 @@ use Illuminate\Http\{Request, JsonResponse};
 class NewsletterController extends Controller
 {
 	/**
-	 * Display a listing of the resource.
-	 */
-	public function index()
-	{
-		return response()->json(['status' => 'Hello']);
-	}
-
-	/**
-	 * Store a newly created resource in storage.
+	 * Subscribe to LCF Mailing List.
 	 */
 	public function store(Request $request): JsonResponse
 	{
-		// Validate the request...
-
-		$subscriber = Newsletter::updateOrCreate([
-			'email' => $request->email
+		$validated = $request->validate([
+			'email' => 'required|email:rfc,dns',
 		]);
 
-		return response()->json(['status' => $subscriber->id]);
+		try {
+			$subscriber = Newsletter::updateOrCreate([
+				'email' => $request->email
+			]);
+
+			return response()->json(['success' => true, 'message' => $subscriber->id]);
+		} catch (\Throwable $th) {
+			$subscriber = Newsletter::where([
+				'email' => $request->email
+			])->restore();
+
+			return response()->json(['success' => $subscriber ? true : false, 'message' => $subscriber ? 'resubscribed' : 'resubscription went wrong']);
+		}
+	}
+
+	/**
+	 * Unsubscribe to LCF Mailing List.
+	 */
+	public function destroy(Request $request): JsonResponse
+	{
+		$validated = $request->validate([
+			'email' => 'required|email:rfc,dns',
+		]);
+
+		$subscriber = Newsletter::where([
+			'email' => $request->email
+		])->delete();
+
+		return response()->json(['success' => $subscriber ? true : false, 'message' => $subscriber ? 'unsubscribed' : 'mail not in list']);
 	}
 }
