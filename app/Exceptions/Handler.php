614fc0d74ad207800e043595d61d0b6247773507
Fray Dalton <fray117@exploiter.id>
2023-06-28 08:41:53 +0700

    handle exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,29 +2,38 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+	/**
+	 * The list of the inputs that are never flashed to the session on validation exceptions.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $dontFlash = [
+		'current_password',
+		'password',
+		'password_confirmation',
+	];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
-    public function register(): void
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
-    }
+	/**
+	 * Register the exception handling callbacks for the application.
+	 */
+	public function register(): void
+	{
+		$this->reportable(function (Throwable $e) {
+			//
+		});
+
+		$this->renderable(function (Throwable $e, Request $request) {
+			return response()->json([
+				'status' => $e->getStatusCode(),
+				'message' => $e->getMessage()
+			], $e->getStatusCode());
+		});
+	}
 }
