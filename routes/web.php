47830a69a8f19a06aa5df3ee4c4403ea79ba3515
Fray Dalton <fray117@exploiter.id>
2023-07-01 09:51:33 +0700

    project cleanup

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['CampusFairAPI' => config('app.version')];
+	return ['CampusFairAPI' => config('app.version')];
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
