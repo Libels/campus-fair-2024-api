321b9631ad9f751b41b4d71dc0459c210adb91be
Fray Dalton <fray117@exploiter.id>
2023-07-01 10:51:12 +0700

    cleanup auth prototypes

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,37 +5,42 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
+use Illuminate\Http\{Request, JsonResponse};
+use Illuminate\Support\Facades\{Auth, Hash};
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Handle an incoming registration request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request): Response
-    {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+	/**
+	 * Handle an incoming registration request.
+	 *
+	 * @throws \Illuminate\Validation\ValidationException
+	 */
+	public function store(Request $request): JsonResponse
+	{
+		try {
+			$request->validate([
+				'name' => ['required', 'string', 'max:255', 'unique:' . User::class],
+				'email' => ['required', 'string', 'email:rfc,dns', 'max:255', 'unique:' . User::class],
+				'password' => ['required', 'confirmed', Password::min(8)->letters()->numbers()->mixedCase()],
+			]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+			$user = User::create([
+				'name' => $request->name,
+				'email' => $request->email,
+				'password' => Hash::make($request->password),
+			]);
 
-        event(new Registered($user));
+			event(new Registered($user));
 
-        Auth::login($user);
+			Auth::login($user);
 
-        return response()->noContent();
-    }
+			return response()->json($request);
+		} catch (\Throwable $th) {
+			return response()->json([
+				"success" => false,
+				"message" => $th->getMessage()
+			]);
+		}
+	}
 }
